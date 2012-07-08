2e231f6626f051bdf7577d968c2b4b7cd6bff5ff
Praveen Selvam <praveen.selvam@gmail.com>
2012-07-08 19:45:46 +0530

    Setup different config yml files for different environments.

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,4 +1,4 @@
 require 'yaml'
 
-yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
+yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', Rails.env + '_config.yml'))).result)
 APP_CONFIG = HashWithIndifferentAccess.new(yaml_data)
